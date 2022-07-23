ce474c22d997460f180ff2e41131d8f63ea4e9e2
tptin76 <tptin.76@gmail.com>
2022-07-24 00:33:51 +0700

    doing map

---
+++ src/TileMap/TileMap.cpp
@@ -0,0 +1,5 @@
+#include "TileMap.h"
+
+TileMap::TileMap()
+{
+}
---
+++ src/TileMap/TileMap.h
@@ -0,0 +1,11 @@
+#ifndef __TILEMAP_H__
+#define __TILEMAP_H__
+
+class TileMap
+{
+private:
+    /* data */
+public:
+    TileMap();
+};
+#endif // __TILEMAP_H__
