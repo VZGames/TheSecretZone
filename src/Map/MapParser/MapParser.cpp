57d26c8f3f3b0ee29ca6b2d459b35d960de53763
tptin76 <tptin.76@gmail.com>
2022-07-28 00:43:40 +0700

    implement tinyxml2

--- src/Map/MapParser/MapParser.cpp
+++ src/Map/MapParser/MapParser.cpp
@@ -8,9 +8,22 @@ MapParser::MapParser()
 bool MapParser::loadMap(const char *p_MapID, const char *p_Source)
 {
     XMLDocument doc;
-    XMLError result = doc.LoadFile(p_Source);
-    if (result == XML_SUCCESS)
+    doc.LoadFile(p_Source);
+    if (doc.ErrorID() == XML_SUCCESS)
     {
+        XMLElement *root = doc.RootElement();
+        int root_Width, root_Height;
+        int tile_Width, tile_Height;
+        root_Width = root->IntAttribute("width");
+        root_Height = root->IntAttribute("height");
+        tile_Width = root->IntAttribute("tilewidth");
+        tile_Height = root->IntAttribute("tileheight");
+
+        GameMap *gameMap = new GameMap();
+
+        m_DictMaps[p_MapID] = gameMap;
+        doc.Clear();
+        delete gameMap;
         return 1;
     }
     return 0;
@@ -18,4 +31,5 @@ bool MapParser::loadMap(const char *p_MapID, const char *p_Source)
 
 void MapParser::parse(const char *p_MapID)
 {
+    m_DictMaps[p_MapID];
 }
